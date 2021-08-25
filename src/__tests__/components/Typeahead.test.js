Huulivoide/react-bootstrap-typeahead

commit 683c2687d4d706e42112749fba0d6aba4f0fa1d9

Trigger `onInputChange` when clicking the clear button (#594)

src/core/Typeahead.js
@@ -290,6 +290,21 @@ export function toggleMenu(state: TypeaheadState, props: Props) {
   return state.showMenu ? hideMenu(state, props) : { showMenu: true };
 }
 
+/**
+ * Manually trigger the input's change event.
+ * https://stackoverflow.com/questions/23892547/what-is-the-best-way-to-trigger-onchange-event-in-react-js/46012210#46012210
+ */
+function triggerInputChange(input: HTMLInputElement, value: string) {
+  const inputValue = Object.getOwnPropertyDescriptor(
+    window.HTMLInputElement.prototype,
+    'value'
+  );
+
+  inputValue && inputValue.set && inputValue.set.call(input, value);
+  const e = new Event('input', { bubbles: true });
+  input.dispatchEvent(e);
+}
+
 class Typeahead extends React.Component<Props, TypeaheadState> {
   static propTypes = propTypes;
   static defaultProps = defaultProps;
@@ -453,7 +468,8 @@ class Typeahead extends React.Component<Props, TypeaheadState> {
   }
 
   _handleClear = () => {
-    this.setState(clearTypeahead, () => this._handleChange([]));
+    this.inputNode && triggerInputChange(this.inputNode, '');
+    this.setState(clearTypeahead);
   }
 
   _handleFocus = (e: SyntheticEvent<HTMLInputElement>) => {

src/__tests__/components/Typeahead.test.js
@@ -1263,21 +1263,29 @@ describe('<Typeahead> `change` events', () => {
     }
   );
 
-  it('calls `onChange` when clicking the clear button', () => {
-    const selected = states.slice(0, 1);
+  it('calls change events when clicking the clear button', () => {
+    let event, value;
+
+    onInputChange = jest.fn((v, e) => {
+      value = v;
+      event = e;
+    });
+
     render(
       <TestComponent
         clearButton
         onChange={onChange}
         onInputChange={onInputChange}
-        selected={selected}
+        selected={states.slice(0, 1)}
       />
     );
 
     screen.getByRole('button').click();
 
     expect(onChange).toHaveBeenCalledTimes(1);
-    expect(onInputChange).toHaveBeenCalledTimes(0);
+    expect(onInputChange).toHaveBeenCalledTimes(1);
+    expect(value).toBe('');
+    expect(event).toBeDefined();
   });
 
   it('calls `onInputChange` when text is entered in the input', () => {

example/src/examples/BasicExample.js
@@ -16,9 +16,11 @@ const BasicExample = () => {
       <Form.Group>
         <Form.Label>Single Selection</Form.Label>
         <Typeahead
+          clearButton
           id="basic-typeahead-single"
           labelKey="name"
           onChange={setSingleSelections}
+          onInputChange={console.log}
           options={options}
           placeholder="Choose a state..."
           selected={singleSelections}

commit 9b8abda7cad745bc00f94193b3f21313dc1eec52

Always call onChange when clear button is pressed.
Commit 683c2687d4d706e42112749fba0d6aba4f0fa1d9 broke this on multi
select mode. As result the internal state of the Typeahead component
was cleared and it appeared empty, but later refocusing the element,
would cause the previously selected options to show up again, as the
clearing had not been propagated to the parent component.

src/core/Typeahead.js
@@ -469,7 +469,12 @@ class Typeahead extends React.Component<Props, TypeaheadState> {
 
   _handleClear = () => {
     this.inputNode && triggerInputChange(this.inputNode, '');
-    this.setState(clearTypeahead);
+    this.setState(clearTypeahead, () => {
+      // Clearing the input fields triggers this for single select mode.
+      if (this.props.multiple) {
+        this._handleChange();
+      }
+    });
   }
 
   _handleFocus = (e: SyntheticEvent<HTMLInputElement>) => {

src/__tests__/components/Typeahead.test.js
@@ -1267,7 +1267,7 @@ describe('<Typeahead> `change` events', () => {
     }
   );
 
-  it('calls change events when clicking the clear button', () => {
+  it('calls change events when clicking the clear button on single', () => {
     let event, value;
 
     onInputChange = jest.fn((v, e) => {
@@ -1285,6 +1285,33 @@ describe('<Typeahead> `change` events', () => {
     );
 
     screen.getByRole('button').click();
+
+    expect(onChange).toHaveBeenCalledTimes(1);
+    expect(onInputChange).toHaveBeenCalledTimes(1);
+    expect(value).toBe('');
+    expect(event).toBeDefined();
+  });
+
+  it('calls change events when clicking the clear button on multiple', () => {
+    let event, value;
+
+    onInputChange = jest.fn((v, e) => {
+      value = v;
+      event = e;
+    });
+
+    render(
+      <TestComponent
+        multiple
+        clearButton
+        defaultInputValue="test"
+        onChange={onChange}
+        onInputChange={onInputChange}
+        selected={states.slice(0, 1)}
+      />
+    );
+
+    screen.getByLabelText('Clear').click();
 
     expect(onChange).toHaveBeenCalledTimes(1);
     expect(onInputChange).toHaveBeenCalledTimes(1);